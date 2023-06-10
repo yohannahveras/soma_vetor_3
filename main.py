b8d158f5922f9e9a4a6ac8033e50a62d8677ad29
Yohannah Véras <yohannahlv@gmail.com>
2023-06-10 17:57:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+n = int(input("Quantos numeros voce vai digitar? "))
+vet = [0 for x in range(n)]
+
+for i in range(0, n):
+    vet[i] = float(input("Digite um numero: "))
+
+print()
+print("VALORES: ", end="")
+for i in range(0, n):
+    print(f"{vet[i]:.1f} ", end="")
+
+print()
+
+soma = 0
+for i in range(0, n):
+    soma = soma + vet[i]
+
+print(f"SOMA = {soma:.2f}")
+
+media = soma / n
+print(f"MEDIA = {media:.2f}")
